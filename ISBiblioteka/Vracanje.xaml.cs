19569a9482fb684c42aa974b8bee5b8d783d168e
mario blagojevic <Maki97_2@hotmail.com>
2019-05-11 17:26:13 +0200

    Citanje zaduzenja kroz view, prepravka baze

--- ISBiblioteka/Vracanje.xaml.cs
+++ ISBiblioteka/Vracanje.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Data;
+using System.Data.SQLite;
 
 namespace ISBiblioteka
 {
@@ -22,12 +24,28 @@ namespace ISBiblioteka
         public Vracanje()
         {
             InitializeComponent();
-            WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
+            UcitajZaduzenja();
         }
 
         private void Dugme_Otkazi_Click(object sender, RoutedEventArgs e)
         {
             Close();
+        }
+
+        private void UcitajZaduzenja()
+        {
+            SQLiteConnection sql_con = new SQLiteConnection("Data Source=db_ISBiblioteka.db;Version=3;");   //podesavanje konekcije
+            sql_con.Open(); //otvaranje kenekcije
+            SQLiteCommand sql_cmd = sql_con.CreateCommand();    //podesavanje komandnog objekta na konekciju
+            sql_cmd.CommandText = "SELECT * FROM pogled_zaduzenja"; //u komandni objekat saljemo sql zahtev
+            SQLiteDataAdapter DB = new SQLiteDataAdapter(sql_cmd.CommandText, sql_con); //sql adapter db izvrsava komandnu odnonso u sebi cuva rezultate sql zahteva (ucitao sve podatke u sebe)
+            sql_con.Close();    //zatavaramo konekciju
+            DataSet DS = new DataSet();     //kreiramo objekta data set koji ce da cuva poadtke iz data dabtera
+
+            DB.Fill(DS);    //dataset punimo podacima iz adaptera
+
+
+            vracanjeListView.ItemsSource = DS.Tables[0].DefaultView;   //data set podatke ispisujemo u listview
         }
         
     }
