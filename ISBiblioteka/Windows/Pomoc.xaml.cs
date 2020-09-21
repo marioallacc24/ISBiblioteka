3e0e120d49266298804361e875af908c3f2af8f0
RioPC <RioPC@DESKTOP-I2B3LNH>
2020-09-21 17:16:35 +0200

    Precice za sve tastere

--- ISBiblioteka/Windows/Pomoc.xaml.cs
+++ ISBiblioteka/Windows/Pomoc.xaml.cs
@@ -25,6 +25,33 @@ namespace ISBiblioteka.Windows
         {
             InitializeComponent();
             WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
+            AddHotKeys();
+        }
+
+        private void AddHotKeys()
+        {
+            try
+            {
+                RoutedCommand firstSettings = new RoutedCommand();
+                firstSettings.InputGestures.Add(new KeyGesture(Key.G, ModifierKeys.Alt));
+                CommandBindings.Add(new CommandBinding(firstSettings, Dugme_GitHub_Click));
+
+                RoutedCommand secondSettings = new RoutedCommand();
+                secondSettings.InputGestures.Add(new KeyGesture(Key.L, ModifierKeys.Alt));
+                CommandBindings.Add(new CommandBinding(secondSettings, Dugme_Linkedin_Click));
+
+                RoutedCommand thirdSettings = new RoutedCommand();
+                thirdSettings.InputGestures.Add(new KeyGesture(Key.S, ModifierKeys.Alt));
+                CommandBindings.Add(new CommandBinding(thirdSettings, Dugme_Sajt_Click));
+
+                RoutedCommand fourthSettings = new RoutedCommand();
+                fourthSettings.InputGestures.Add(new KeyGesture(Key.O, ModifierKeys.Alt));
+                CommandBindings.Add(new CommandBinding(fourthSettings, Dugme_O_Click));
+            }
+            catch (Exception err)
+            {
+                System.Windows.MessageBox.Show(err.Message);
+            }
         }
 
         private const string sHTMLHelpFileName = "isBiblioteka.chm";
